916483a45c41aeb04c375d8d56babdf2e72dfb7b
CondesnsedMilk <achau103@student.hbuhsd.edu>
2025-01-09 21:44:28 +0000

    Commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace unit_4_5_assignment_CondesnsedMilk;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> firstlist = new List<int>();
+
+        firstlist.Add(1);
+        firstlist.Add(2);
+        firstlist.Add(3);
+        firstlist.Add(3);
+
+
+        Console.WriteLine("Capacity Is: " + firstlist.Capacity);
+
+        Console.WriteLine("Count Is: " + firstlist.Count);
+
+        firstlist.Add(5);
+        firstlist.Add(3);
+        firstlist.Add(3);
+        firstlist.Add(3);
+        firstlist.Add(3);
+        firstlist.Add(3);
+        firstlist.Add(3);
+        firstlist.Add(3);
+        firstlist.Add(3);
+        firstlist.Add(3);
+
+
+        firstlist.TrimExcess();
+
+        Console.WriteLine("Capacity Is: " + firstlist.Capacity);
+
+
+        Console.WriteLine("Count Is: " + firstlist.Count);
+
+        firstlist.ForEach(Console.WriteLine);
+    }
+}
